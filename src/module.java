59c5b46b59d996580936bb4ec1c1f5857be64bf7
veraDulda <99422897+veraDulda@users.noreply.github.com>
2022-04-23 17:17:30 +0300

    module için 1.parti çalışma

--- src/module.java
+++ src/module.java
@@ -1,5 +1,6 @@
 public class module {
     public static void main(String[] args) {
         System.out.println("module için dosya oluşturma");
+        System.out.println("module için 1.parti çalışma");
     }
 }
