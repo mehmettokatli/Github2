5f4bd0c5332171936ceeb97166865c68240bb14b
veraDulda <99422897+veraDulda@users.noreply.github.com>
2022-04-23 17:10:10 +0300

    öğleden sonra çalışması

--- src/login.java
+++ src/login.java
@@ -2,5 +2,6 @@ public class login {
     public static void main(String[] args) {
         System.out.println("Halil tarafından eklenen dosya");
         System.out.println("login iç,n öğleden önce çalışması");
+        System.out.println("login iç,n öğleden sonra çalışması");
     }
 }
